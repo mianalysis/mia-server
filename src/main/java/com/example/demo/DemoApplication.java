05ab85b7a77796ba4f11d8ae7ffb4f180f5e7d4f
jdjfisher <43887886+jdjfisher@users.noreply.github.com>
2023-10-30 19:22:05 +0000

    Moved demo route into Controller

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,59 +1,12 @@
 package com.example.demo;
 
-import java.io.File;
-
-import javax.annotation.Resource;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
-import io.github.mianalysis.mia.module.Modules;
-import io.github.mianalysis.mia.object.Workspace;
-import io.github.mianalysis.mia.object.Workspaces;
-import io.github.mianalysis.mia.object.image.Image;
-import io.github.mianalysis.mia.process.analysishandling.AnalysisReader;
 
 @SpringBootApplication
-@RestController
 public class DemoApplication {
-	private ServerImageRenderer serverImageRenderer = new ServerImageRenderer();
-
-	@Resource(name = "getWorkspace")
-	Workspace workspace;
-
-	@Resource(name = "getModules")
-	Modules modules;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-	}
-
-	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
-	}
-
-	@GetMapping("/mia")
-	public @ResponseBody ResponseEntity<byte[]> mia(@RequestParam(value = "threshold", defaultValue = "1.0") String threshold) throws Exception {
-		Image.setDefaultRenderer(serverImageRenderer);
-		serverImageRenderer.clearLastOutput();
-
-		modules.getModuleByID("1636961828208").updateParameterValue("Threshold multiplier", Float.parseFloat(threshold));
-		modules.execute(workspace);
-
-		while (serverImageRenderer.getLastOutputImage() == null)
-			Thread.sleep(10);
-
-		return ResponseEntity.ok()
-            .contentType(MediaType.IMAGE_PNG)
-            .body(serverImageRenderer.getLastOutputImage());
 	}
 }
